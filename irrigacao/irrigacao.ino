814db9b992c2b17d2bfcd74c82f06f5f0d067a4a
Treepo <thiago.freire45@gmail.com>
2022-05-20 01:09:48 -0300

    bot finalizado

--- irrigacao/irrigacao.ino
+++ irrigacao/irrigacao.ino
@@ -3,29 +3,79 @@
 #include <ArduinoJson.h>
 
 
-bool leituraSensor;
-bool leituraAnterior;
+
+bool modoAuto;
+int leituraSensorIntervalo = 20; //86400 (24 horas)
+long ultimaLeituraSensor = 0;
+int leituraSensor = 0;
 
 const char* ssid     = "BETHO&CRIS";     // your network SSID (name of wifi network)
 const char* password = "carvalhosobral5545"; // your network password
 
 
-#define LED_VERMELHO 5
-#define LED_AMARELO 6
-#define LED_VERDE 7
-#define SENSOR_UMIDADE 8
+
+
+
+#define SENSOR_UMIDADE 32
 #define ATUADOR 12
 #define BOTtoken "5117193859:AAECeNChQFSTdlW00K8P7iORndw0I3G125c"
 #define CHAT_ID "1383689596"
 
+#define valorCriticoUmidade 2000
+
 WiFiClientSecure client;
 UniversalTelegramBot bot(BOTtoken, client);
+
+
+int readSensor(){
+  int readed = analogRead(SENSOR_UMIDADE);
+  Serial.print("*** Nível de umidade (0-4095): ");
+  Serial.println(4095 - readed);
+  return 4095 - readed;
+}
+
+
+void handle_irrig_auto(){
+
+        if (leituraSensor < valorCriticoUmidade) {
+        
+        String res = "Umidade abaixo do limite, ativando irrigador\n";
+        res+="Nível de umidade [0-4095]: ";
+        res+=String(leituraSensor);
+        bot.sendMessage(CHAT_ID, res,""); // informando via telegram sobre a umidade
+        delay(5000);
+        while(readSensor() < valorCriticoUmidade && modoAuto){
+        digitalWrite(ATUADOR, HIGH); //ativando irrigacao
+        delay(500);
+        digitalWrite(ATUADOR, LOW); //desativando irrigacao
+
+        delay(10000); //esperando pela mudança da umidade
+    }
+    bot.sendMessage(CHAT_ID, "Irrigador desativado :)","");
+        }
+  
+  }
+
+void handle_irrig(){
+
+  ultimaLeituraSensor = millis();
+      
+         String res = "Nível de umidade [0-4095]: ";
+         res+=String(leituraSensor);
+         if (leituraSensor < valorCriticoUmidade){
+         res+=" Me regue :( ";
+         }
+         bot.sendMessage(CHAT_ID, res,""); // informando via telegram sobre a umidade
+  
+  
+}
 
 
 
 void handleNewMessages(int numNewMessages) {
   Serial.println("handleNewMessages");
   Serial.println(String(numNewMessages));
+
 
   for (int i=0; i<numNewMessages; i++){
     String chat_id = String(bot.messages[i].chat_id);
@@ -46,20 +96,31 @@ void handleNewMessages(int numNewMessages) {
       welcome+= "/off para desativar o controle automático de irrigação\n";
       welcome+= "/check para checar a umidade atual do solo\n";
       bot.sendMessage(chat_id, welcome, "");
+      modoAuto = false;
     }
     if (text == "/on") {
       bot.sendMessage(chat_id, "Sistema de irrigação automática ligado!", "");
-    }
+      modoAuto = true;
+  }
+    
      if (text == "/off") {
       bot.sendMessage(chat_id, "Sistema de irrigação automática desligado!", "");
+      modoAuto = false;
     }
 
      if (text == "/check") {
-      bot.sendMessage(chat_id, "A umidade atual do solo é de " , "");
+      String res = "Nível de umidade [0-4095]: ";
+      int umidadeAtual = readSensor();
+       res+=String(umidadeAtual);
+       if(umidadeAtual < valorCriticoUmidade){
+        res+=" Me regue! :(";
+       }
+      bot.sendMessage(chat_id, res , "");
+      
+    }
     }
   }
   
-}
 
 
 //checando novas mensagens a cada 1 segundo
@@ -88,12 +149,20 @@ void setup() {
 
   client.setCACert(TELEGRAM_CERTIFICATE_ROOT);
 
+  pinMode(ATUADOR, OUTPUT);
+
  
 
   }
 
 
 void loop() {
+    leituraSensor = readSensor();
+    
+    if (!modoAuto && millis()> ultimaLeituraSensor + ((leituraSensorIntervalo)*1000) or ultimaLeituraSensor == 0) {handle_irrig();}
+    if (modoAuto ){handle_irrig_auto();}
+
+    
   if (millis() > lastTimeBotCheck + botRequestDelay) {
     int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
 
